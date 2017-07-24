1903d12c3da4b6506b78fcd9ef6105ceabea7055
arr15334 <arr15334@uvg.edu.gt>
2017-07-23 18:27:10 -0600

    atributos, sets y gets

--- Jugueteria/src/Jueguete.java
+++ Jugueteria/src/Jueguete.java
@@ -9,5 +9,57 @@
  * @author Rodrigo
  */
 public class Jueguete {
+    private String codigo, marca;
+    private double valorUnitario, valorAgregado;
+    private int edadMinima, numProveedor;
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public double getValorUnitario() {
+        return valorUnitario;
+    }
+
+    public void setValorUnitario(double valorUnitario) {
+        this.valorUnitario = valorUnitario;
+    }
+
+    public double getValorAgregado() {
+        return valorAgregado;
+    }
+
+    public void setValorAgregado(double valorAgregado) {
+        this.valorAgregado = valorAgregado;
+    }
+
+    public int getEdadMinima() {
+        return edadMinima;
+    }
+
+    public void setEdadMinima(int edadMinima) {
+        this.edadMinima = edadMinima;
+    }
+
+    public int getNumProveedor() {
+        return numProveedor;
+    }
+
+    public void setNumProveedor(int numProveedor) {
+        this.numProveedor = numProveedor;
+    }
+    
     
 }
