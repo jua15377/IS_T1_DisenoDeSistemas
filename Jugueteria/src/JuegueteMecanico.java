42e487e4e8b52589055895b85def538a3f4d0aab
arr15334 <arr15334@uvg.edu.gt>
2017-07-23 18:33:12 -0600

    clase juguete mecanico con constructor sets y gets

---
+++ Jugueteria/src/JuegueteMecanico.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Rodrigo
+ */
+public class JuegueteMecanico extends Jueguete{
+    private int complejidad;
+    private double vAdicional;
+
+    public JuegueteMecanico(String codigo, String marca, double valorUnitario, double valorAgregado, int edadMinima, int numProveedor) {
+        super(codigo, marca, valorUnitario, valorAgregado, edadMinima, numProveedor);
+    }
+
+    public int getComplejidad() {
+        return complejidad;
+    }
+
+    public void setComplejidad(int complejidad) {
+        this.complejidad = complejidad;
+    }
+
+    public double getvAdicional() {
+        return vAdicional;
+    }
+
+    public void setvAdicional(double vAdicional) {
+        this.vAdicional = vAdicional;
+    }
+
+    public double valorFinal(int complejidad, double valorUnitario, double vAdicional){
+        double vFinal = valorUnitario + vAdicional*complejidad;
+        return vFinal;
+    }
+}
